96f33fdf6b61a31aaab3fb5c1b31e57920873e52
kwonjunpyo <70629241+kwonjunpyo@users.noreply.github.com>
2020-09-16 17:22:00 +0900

    Add files via upload

--- aa2-20-rpt02/client.js
+++ aa2-20-rpt02/client.js
@@ -1,16 +1,18 @@
 // tcp client
-var net = require('net');
+var net = require("net");
 var port = 3000;
-var client = ;
+var client = new net.Socket();
 
 // Connection using socket
-client.(port, "127.0.0.1");
+client.connect(port, "127.0.0.1");
 
 // Receive data from socket
-
-
-// Add a 'close' event handler for the client socket
-client.on('close', function () {
-    console.log('Connection closed');
+client.on("data", function (data) {
+  console.log("Data: " + data);
+  client.destroy();
 });
 
+// Add a 'close' event handler for the client socket
+client.on("close", function () {
+  console.log("Connection closed");
+});
--- aa2-20-rpt02/server.js
+++ aa2-20-rpt02/server.js
@@ -1,12 +1,12 @@
 // tcp server (network server)
-var net = require();
+var net = require("net");
 var port = 3000;
 
 // Network connection using socket
-var server = net.createServer(function(  ) {
-	console.log("Connection from " + socket.);
-	socket.end("Hello AA00! from localhost:3000");
+var server = net.createServer(function (socket) {
+  console.log("Connection from " + socket.remoteAddress);
+  socket.end("Hello AA20! from localhost:3000");
 });
 
-server.(port, "127.0.0.1");
+server.listen(port, "127.0.0.1");
 console.log("Network server started at port : " + port);
